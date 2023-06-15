a88ca04b7167c5f982af885c9654f7afc192aa23
chase-michael <chase.zelechowski@gmail.com>
2023-06-15 15:20:06 -0400

    Cosmetic changes, Dev Upload form, bug fix
    I made some cosmetic changes to the logo and website metadata, fixed a bug in Sign Out, and created a form to manually upload sample artwork.

--- src/utils/handleSignOut.js
+++ src/utils/handleSignOut.js
@@ -1,9 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 
 function handleSignOut(navigate) {
-    localStorage.removeItem('authToken');
-    navigate('/');
-    window.scrollTo(0, 0);
+    if (localStorage.getItem('authToken')) {
+        localStorage.removeItem('authToken');
+        navigate('/');
+        window.scrollTo(0, 0);
+    }
 }
 
 export default handleSignOut;
