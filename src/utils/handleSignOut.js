965d7af0e3d1b941d8bac9657cedf90c33b96695
Chase <chasez95@gmail.com>
2023-08-16 12:51:27 -0400

    Deploy React app to GitHub Pages

--- src/utils/handleSignOut.js
+++
@@ -1,9 +0,0 @@
-function handleSignOut(navigate) {
-    if (localStorage.getItem('authToken')) {
-        localStorage.removeItem('authToken');
-        localStorage.removeItem('userData');
-        navigate('/');
-    }
-}
-
-export default handleSignOut;
