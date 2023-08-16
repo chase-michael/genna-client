b05946475157d0f196e58e1e061c701abcc63787
chase-michael <chase.zelechowski@gmail.com>
2023-08-16 13:22:39 -0400

    history

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import './styles/index.css';
 import App from './App';
 import { UserInputProvider } from './contexts/UserInputContext';
 import { AuthProvider } from './contexts/AuthContext';
-
+import { createBrowserHistory } from 'history';
+export const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
